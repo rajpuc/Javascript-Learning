e4548ff577465d452bce00e72c9382d2ec5f7233
rajesh <ajoypal.raj@gmail.com>
2024-11-30 18:24:06 +0600

    c8: all aout object

---
+++ 03/index.js
@@ -0,0 +1,17 @@
+/**
+ * ! Object er moddhe quotation(" ") key te dite o pare na o dite pare. but value jodi string hoi tokon dite hobe.
+ */
+
+var linkdin={
+    name:"Rajesh Pal",
+    designation:"Student",
+    company:"IDLC",
+    Followers:6705,
+    post_impression:"2570",
+    is_bangladeshi: true
+}
+console.log(linkdin);
+console.log(linkdin.name);
+console.log(linkdin.designation);
+console.log(linkdin.company);
+console.log(linkdin.Followers);
