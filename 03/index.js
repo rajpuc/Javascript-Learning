dd5007d0fbc55186be753b29c12500292bac4716
rajesh <ajoypal.raj@gmail.com>
2024-11-30 19:01:31 +0600

    c10: Loop over array

--- 03/index.js
+++ 03/index.js
@@ -1,29 +1,2 @@
-/**
- * ! ai example er aghe amra j object baniyecilam sheta k json object o bola hoi.
- * ! What is json array?
- * * Jokon akhidoroner koyekta object array er bithore kotaw teke niye ashi tokon sheta k json array bola hoi.
- * ! Use case of Json array: Jokon frontend teke backend a eksathe onkgula obj patanor proyojon pore ba backend teke frontend a onk object eksthe annar proyojon pore thokon amra Json array use kori.
- */
 
-var jsonArray = [
-    {
-        title:"JS",
-        description: "Hello Lucifer"
-    },
-    {
-        title:"Laravel",
-        description: "Hello Lucifer"
-    },
-    {
-        title:"Mongodb",
-        description: "Hello Lucifer"
-    },
-    {
-        title:"express JS",
-        description: "Hello Lucifer"
-    }
-]
 
-console.log(jsonArray);
-console.log(jsonArray[2]);
-console.log(jsonArray[2].title);
